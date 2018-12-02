d4738a51a1dc76958ceeaf81f76094151921252a
Samit Khalsa <skhalsa@uwo.ca>
2018-12-02 20:20:04 +0000

    Setting up Front End

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+import firestore from 'firebase/firestore';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'frontend';
+}
